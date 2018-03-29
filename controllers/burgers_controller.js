fe13b51920d8c791298f5538cc4b7ffe4321e75a
FuzzyJones <donald.jones83@gmail.com>
2018-03-29 19:53:44 -0400

    kinda working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,17 +4,42 @@ var burger = require("../models/burger.js");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-    res.render("index");
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+    //   console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
   });
-
-// router.get("/", function(req, res) {
-//     connection.query("SELECT * FROM burgers;", function(err, data) {
-//         if (err) {
-//             return res.status(500).end();
-//         }
-//         res.render("index", { burgers: data });
-//         });
-//   });
+  
+  router.post("/api/burgers", function(req, res) {
+    burger.create(["name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+     
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    // console.log("condition", condition);
+  
+    burger.update(
+      {
+        devoured: req.body.devoured
+      },
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      }
+    );
+  });
 
 
 module.exports = router;
