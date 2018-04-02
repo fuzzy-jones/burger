16c44239316739d98f0fc78fcff90a19477d9ec6
FuzzyJones <donald.jones83@gmail.com>
2018-04-01 20:15:34 -0400

    adding new burger, need to devour on click

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,34 +1,39 @@
 $(function() {
 
     $("#devburger").on("click", function(event) {
-        var id = $(this).data("id");
-        var newDevour = $(this).data("newdevour");
+        event.preventDefault();
+        
+        var id = $(this).data("burgerid");
+        var name = $(this).data("burgername");
+        
     
         var newDevourBurger = {
-            devoured: newDevour
+            id: id,
+            burger_name: name,
+            devoured: 0
         };
-    
-        // Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: newDevourBurger
-        }).then(
-            function() {
-            console.log("changed devour to", newDevoured);
-            // Reload the page to get the updated list
-            location.reload();
-            }
-        );
+        
+        console.log(newDevourBurger)
+        // send the put request
+        // $.ajax("/api/burgers/" + id, {
+        //     type: "PUT",
+        //     data: newDevourBurger
+        // }).then(
+        //     function() {
+        //     console.log("changed devour to", newDevour);
+        //     // Reload the page to get the updated list
+        //     location.reload();
+        //     }
+        // );
     });
 
 
     $("#addburger").on("submit", function(event) {
-        // Make sure to preventDefault on a submit event.
         event.preventDefault();
         
         var newBurger = {
-            burger_name: $("#burg").val().trim()
-            devoured: false
+            burger_name: $("#burg").val().trim(),
+            devoured: 1
         };
 
         // Send the POST request.
